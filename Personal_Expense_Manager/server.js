7d9805232e7c86a8718d5c5cc049d37a768ab1e3
Chetan Kolhe <Chetankolhe1020@gmail.com>
2025-03-02 17:24:48 +0530

    t

--- Personal_Expense_Manager/server.js
+++ Personal_Expense_Manager/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import Login from './Routes/index.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
